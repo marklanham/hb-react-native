f905edfbb6043cbd5944389ecef6c9ac37f33453
Mark Lanham <m.lanham@aitch-c.com>
2022-04-30 01:56:55 +0100

    Configured node to call JSON file with filter option

--- server/src/controllers/products.ts
+++ server/src/controllers/products.ts
@@ -1,13 +1,19 @@
 import express from 'express'
+import * as data from '../db/data/products.json'
 
 const router = express.Router()
 
 router.get('/all', (_, res) => {
-  res.json({})
+  res.json({ data })
 })
 
-router.get('/:id', (_, res) => {
-  res.json({})
+router.get('/:id', function (req, res) {
+  const containsId = new RegExp(req.params.id)
+  res.json(
+    Object.values(data).filter((item) => {
+      return containsId.test(item.id)
+    }),
+  )
 })
 
 export default router
